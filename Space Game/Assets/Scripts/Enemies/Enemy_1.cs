2453cbb5c5823922564420664ac67d641d8feb23
mr.devpotato <67113818+over1548@users.noreply.github.com>
2022-06-22 19:16:10 +0200

    Update Enemy_1.cs

--- Space Game/Assets/Scripts/Enemies/Enemy_1.cs
+++ Space Game/Assets/Scripts/Enemies/Enemy_1.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Enemy_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float enemyWalkSpeed;
+    [HideInInspector]
+    public bool patrol;
+    private bool flip;
+
+    public Rigidbody2D rb2d;
+    public Transform groundCheckPos;
+    public Collider2D bodyColl;
+    public LayerMask groundLayer;
+    public LayerMask wallLayer;
+
     void Start()
     {
-        
+        patrol = true; 
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (patrol)
+        {
+            flip = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+        }
+    }
+
     void Update()
     {
-        
+        if (patrol)
+        {
+            Patrol();
+        }
     }
+
+    public void Patrol()
+    {
+        if (flip || bodyColl.IsTouchingLayers(wallLayer))
+        {
+            Flip();
+        }
+        rb2d.velocity = new Vector2(enemyWalkSpeed * Time.fixedDeltaTime, rb2d.velocity.y);
+    }
+
+    void Flip()
+    {
+        patrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        enemyWalkSpeed *= -1;
+        patrol = true;
+    }
+    
 }
