1151fb31dad58080b86107716dc58f87c4f57fc1
carrotboardgame <75209990+carrotboardgame@users.noreply.github.com>
2022-06-22 22:00:32 +0700

    Laser added
    Added laser that detect the player and turn on and off every 3 seconds.

---
+++ Space Game/Assets/Scripts/Hazards/LaserHitting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserHitting : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private BoxCollider2D box;
+    //Hitting the player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //Consequences for touching the laser
+        }
+    }
+    IEnumerator start()
+    {
+        Debug.Log("getting the signal");
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(5f);
+            sprite.enabled = false;
+            box.enabled = false;
+            yield return new WaitForSecondsRealtime(5f);
+            sprite.enabled = true;
+            box.enabled = true;
+            
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine("start");
+        Debug.Log("HaveStart");
+    }
+}
