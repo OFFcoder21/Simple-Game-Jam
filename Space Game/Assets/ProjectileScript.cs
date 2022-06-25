e149d8337831d32354958cc387b241002ef26709
mr.devpotato <67113818+over1548@users.noreply.github.com>
2022-06-25 16:43:14 +0200

    smthings

--- Space Game/Assets/ProjectileScript.cs
+++ Space Game/Assets/ProjectileScript.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileScript : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
     public float waitDestroyTime;
+    float speed = 500f;
+    Rigidbody2D rb2d;
+    public GameObject target;
+    Vector2 moveDir;
     // Start is called before the first frame update
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+        moveDir = (target.transform.position - transform.position).normalized * speed;
+        rb2d.velocity = new Vector2(moveDir.x, moveDir.y);
         StartCoroutine(DestroyBulletTimer());
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +27,7 @@ public class ProjectileScript : MonoBehaviour
 
     void DestroyProjectile()
     {
+        //ex efx
         Destroy(gameObject);
     }
 
--- Space Game/Assets/Scripts/Enemies/EnemyAI.cs
+++ Space Game/Assets/Scripts/Enemies/EnemyAI.cs
@@ -4,80 +4,13 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public float wait;
-    public float speed;
-    private bool movingRight = true;
-    public Transform groundDetection;
-    public float enemyHealth;
-    
-    private void Update()
+    public void Start()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
-        if(groundInfo == false)
-        {
-            if(movingRight == true)
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
-            }
-        }
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "Obstacle")
-        {
-            if (movingRight == true)
-            {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
-            }
-        }
-        if(collision.collider.tag == "Player")
-        {
-            transform.position = new Vector2(Vector2.zero.x, Vector2.zero.y);
-            StartCoroutine(waitBeforeMoving());
 
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D coll)
+    public void Update()
     {
-        if(coll.tag == "Bullet")
-        {
-            DamageEnemy(1f);
-        }
-        if(coll.tag == "FireHurt")
-        {
-            DamageEnemy(1.5f); ;
-        }
-    }
-
-    void DamageEnemy(float damage)
-    {
-        enemyHealth -= damage;
-        if(enemyHealth <= 0)
-        {
-            //play death anim
-            //(spawn coin)
-            Destroy(gameObject);
-        }
-    }
-
-    IEnumerator waitBeforeMoving()
-    {
-        yield return new WaitForSecondsRealtime(0.7f);
-        yield return null;
+        
     }
 }
